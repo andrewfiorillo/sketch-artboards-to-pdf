34ed1ff2018835ec2fc82be3545a60365e13b9be
Michael Le <mle.general@gmail.com>
2016-06-12 12:29:34 +0100

    Allow for selected artboards to be exported to PDF

--- Artboards To PDF.sketchplugin/Contents/Sketch/main.js
+++ Artboards To PDF.sketchplugin/Contents/Sketch/main.js
@@ -1,3 +1,4 @@
+var app = [NSApplication sharedApplication];
 
 function currentPageToPDF(context) {
 	var doc = context.document;
@@ -11,4 +12,74 @@ function currentPageToPDF(context) {
 function allPagesToPDF(context) {
 	var doc = context.document;
 	doc.exportPDFBook(doc);
+}
+
+function selectedArtboardsToPDF(context){
+	var doc = context.document;
+	var page = doc.currentPage();
+	var pageName = page.name();
+
+	// Check document for selected artboards
+	var selectionLayers = context.selection;
+
+	var hasSelectedArtboards = false;
+	if (doc.hasArtboards() && selectionLayers){
+		log(selectionLayers.count());
+		for (var i=0; i < selectionLayers.count(); i++){
+			if (selectionLayers[i].className() == "MSArtboardGroup"){
+				hasSelectedArtboards = true;
+				break;
+			}
+		}
+	}
+
+	if (!hasSelectedArtboards){
+		[app displayDialog:"Please select an artboard" withTitle:"We couldn't find any selected artboards"];
+		return;
+	}
+
+	// Allow the user to specify filename and location to save
+	var saveDialog = NSSavePanel.savePanel();
+	[saveDialog setNameFieldStringValue:pageName];
+	saveDialog.setAllowedFileTypes(["pdf"]);
+
+	if(saveDialog.runModal() == NSOKButton) {
+		var fileURL = saveDialog.URL();
+		log(fileURL);
+
+		var artboards = doc.artboards();
+		var selectedArtboardsCount = artboards.count();
+
+		var pdf = PDFDocument.alloc().init();
+
+		for (var i=0; i < selectedArtboardsCount; i++){
+			var artboard = artboards[i];
+		    if (artboard.isSelected()){
+
+		    	// To ensure that the filename will be safe
+		    	var name = cleanString(artboard.name());
+
+		    	// To avoid issues with artboards with the same name
+		    	var artboardId = artboard.objectID();
+		       
+		       	// It is quicker and a smaller filesize to export artboards as PDF rather than PNG.
+		       	// However the quality of the PNG export is better.
+		        var path = NSTemporaryDirectory() + artboardId + '/' + name + '.png';
+		        doc.saveArtboardOrSlice_toFile(artboard,path);
+
+		        var image = [[NSImage alloc] initByReferencingFile:path];
+		        var page = [[PDFPage alloc] initWithImage:image];
+
+		        [pdf insertPage:page atIndex:[pdf pageCount]];
+		    }
+		}
+
+		[pdf writeToURL: fileURL];
+	}
+}
+
+function cleanString(string){
+	var notAllowedChars = [NSCharacterSet characterSetWithCharactersInString:@"\\<>=,!#$&'()*+/:;=?@[]%"];
+	var cleanString = [[string componentsSeparatedByCharactersInSet:notAllowedChars] componentsJoinedByString:@""];
+	return cleanString;	
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A Sketch plugin to export artboards to PDF slideshow.
 
-You can export artboards from the current page, or from all pages in the document. The PDF slideshow page order is determined by the descending order of artboards in the layer list.
+You can export artboards from the current page, selected artboards or from all pages in the document. The PDF slideshow page order is determined by the descending order of artboards in the layer list.
