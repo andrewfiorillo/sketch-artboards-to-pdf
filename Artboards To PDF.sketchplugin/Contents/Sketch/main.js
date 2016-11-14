079c518baa29e38daf2ad5205967b4f72d9c8d65
Vyacheslav Dubovitsky <evil.mrfix@gmail.com>
2016-11-14 18:21:50 +0200

    Ability to export only selected artboards as multipage PDF + small refactor and tweaks.

--- Artboards To PDF.sketchplugin/Contents/Sketch/main.js
+++ Artboards To PDF.sketchplugin/Contents/Sketch/main.js
@@ -1,13 +1,37 @@
 
 function currentPageToPDF(context) {
-	var doc = context.document;
-	var page = doc.currentPage();
-	var pageName = page.name();
-	var pageArray = [page];
-	MSPDFBookExporter.exportPages_defaultFilename(pageArray, pageName +".pdf");
+    pageToPDF(context.document.currentPage())
+}
+
+function selectedArtboardsToPDF(context) {
+    var doc = context.document
+    var handler = doc.eventHandlerManager().normalHandler()
+    
+    var tempPage = MSPage.new()
+    doc.documentData().addPage(tempPage)
+    tempPage.setName("PDF Export")
+    
+    var selectionLoop = context.selection.objectEnumerator()
+    var selectedArtboards = NSMutableArray.array()
+    while (layer = selectionLoop.nextObject()) {
+        if (layer.isMemberOfClass(MSArtboardGroup)) {
+            selectedArtboards.addObject(layer.copy())
+        }
+    }
+    tempPage.addLayers(selectedArtboards)
+    
+    doc.documentData().setCurrentPage(tempPage)
+    pageToPDF(tempPage)
+
+    doc.documentData().removePage(tempPage)
 }
 
 function allPagesToPDF(context) {
-	var doc = context.document;
-	doc.exportPDFBook(doc);
+	var doc = context.document
+	doc.exportPDFBook(doc)
+}
+
+function pageToPDF(page) {
+    var pageArray = [page]
+    MSPDFBookExporter.exportPages_defaultFilename(pageArray, page.name() + ".pdf")
 }
