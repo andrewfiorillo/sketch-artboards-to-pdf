bf4a34469a8f5a88edec12d9ec4cad8fa4efd79a
Andrew Fiorillo <fiorilloaj@gmail.com>
2017-04-10 16:46:55 -0400

    fixed hidden symbols showing up in selected artboard export. added comments to code. reformatted and reorganized. improved error handling.

--- Artboards To PDF.sketchplugin/Contents/Sketch/main.js
+++ Artboards To PDF.sketchplugin/Contents/Sketch/main.js
@@ -1,63 +1,111 @@
 
-function currentPageToPDF(context) {
-    pageToPDF(context.document.currentPage())
-}
-
-function selectedArtboardsToPDF(context) {
-    var doc = context.document
-    var selection = context.selection
-    
-    if (selection.count() <= 0) {
-        print(selection)
-        document.showMessage("Please select any artboards to proceed")
-        return
-    }
-    
-    var tempPage = MSPage.new()
-    doc.documentData().addPage(tempPage)
-    tempPage.setName("PDF Export")
-    
-    var selectionLoop = selection.objectEnumerator()
-    var selectedArtboards = NSMutableArray.array()
-    while (layer = selectionLoop.nextObject()) {
-        if (layer.isMemberOfClass(MSArtboardGroup)) {
-            selectedArtboards.addObject(layer.copy())
-        } else if (layer.isMemberOfClass(MSSymbolMaster)) {
-            var layerCopy = MSSymbolMaster.convertSymbolToArtboard(layer.copy())
-            selectedArtboards.addObject(layerCopy)
-        } else {
-            doc.showMessage("Only artboards allowed to proceed PDF export")
-            return
-        }
-    }
-    tempPage.addLayers(selectedArtboards)
-    
-    var pageChildrenLoop = tempPage.children().objectEnumerator()
-    while (pageLayer = pageChildrenLoop.nextObject()) {
-        if (pageLayer.isMemberOfClass(MSSymbolInstance)) {
-            findAndDetachFromSymbol(pageLayer)
-        }
-    }
-    function findAndDetachFromSymbol(layer) {
-        if (layer.isMemberOfClass(MSSymbolInstance)) {
-            layer = layer.detachByReplacingWithGroup()
-            var layerChildrenLoop = layer.children().objectEnumerator()
-            while (innerLayer = layerChildrenLoop.nextObject()) {
-                findAndDetachFromSymbol(innerLayer)
-            }
-        }
-    }
-    
-    pageToPDF(tempPage)
-    doc.documentData().removePage(tempPage)
-}
-
-function allPagesToPDF(context) {
-	var doc = context.document
-	doc.exportPDFBook(doc)
-}
+//-------------------------------------------------------------------------------------------------------------
+// Utilities
+//-------------------------------------------------------------------------------------------------------------
 
 function pageToPDF(page) {
-    var pageArray = [page]
-    MSPDFBookExporter.exportPages_defaultFilename(pageArray, page.name() + ".pdf")
+	var pageArray = [page];
+	MSPDFBookExporter.exportPages_defaultFilename(pageArray, page.name() + ".pdf");
+}
+
+
+//-------------------------------------------------------------------------------------------------------------
+// Export all pages to PDF
+//-------------------------------------------------------------------------------------------------------------
+
+
+function allPagesToPDF(context) {
+	var doc = context.document;
+	doc.exportPDFBook(doc);
+}
+
+
+//-------------------------------------------------------------------------------------------------------------
+// Export current page to PDF
+//-------------------------------------------------------------------------------------------------------------
+
+
+function currentPageToPDF(context) {
+	pageToPDF(context.document.currentPage());
+}
+
+
+//-------------------------------------------------------------------------------------------------------------
+// Export only selected artboards to PDF
+//-------------------------------------------------------------------------------------------------------------
+
+
+function selectedArtboardsToPDF(context) {
+	var doc = context.document;
+	var selection = context.selection;
+	
+	// Check for slection
+	
+	if (selection.length <= 0) {
+		NSApp.displayDialog("No artboards selected!");
+		return;
+	}
+	
+	// Creat temporary page to house selected artboards
+	
+	var tempPage = MSPage.new();
+	doc.documentData().addPage(tempPage);
+	tempPage.setName("PDF Export");
+	
+	// Loop through selection to check for artboards and symbols
+	
+	var selectedArtboards = [];
+	
+	for (var i = 0; i < selection.length; i++) {
+		var layer = selection[i];
+		if (layer.isMemberOfClass(MSArtboardGroup)) {
+			selectedArtboards.push(layer.copy());
+		} else if (layer.isMemberOfClass(MSSymbolMaster)) {
+			var layerCopy = MSSymbolMaster.convertSymbolToArtboard(layer.copy());
+			selectedArtboards.push(layerCopy);
+		} else {
+			NSApp.displayDialog("Only artboards can be exported.");
+			return;
+		}
+	};
+	
+	// Add selected artboards to temporary page
+	
+	tempPage.addLayers(selectedArtboards);
+	
+	// Remove hidden layers
+	
+	var tempLayers = tempPage.children()
+	for (var i = 0; i < tempLayers.length; i++) {
+		var layer = tempLayers[i];
+		if (layer.isVisible() == 0) {
+			layer.removeFromParent();
+		}
+	};
+	
+	// Detach symbols to prevent display bug
+	
+	var pageChildren = tempPage.children();
+	for (var i = 0; i < pageChildren.length; i++) {
+		var layer = pageChildren[i];
+		if (layer.isMemberOfClass(MSSymbolInstance)) {
+			findAndDetachFromSymbol(layer);
+		}
+	};
+	
+	function findAndDetachFromSymbol(layer) {
+		if (layer.isMemberOfClass(MSSymbolInstance)) {
+			var group = layer.detachByReplacingWithGroup();
+			var children = group.children();
+			for (var i = 0; i < children.length; i++) {
+				findAndDetachFromSymbol(children[i]);
+			};
+		}
+	}
+	
+	// Export temporary page, then remove it from document
+	
+	pageToPDF(tempPage);
+	tempPage.removeFromParent();
+	doc.documentData().removePage(tempPage)
 }
